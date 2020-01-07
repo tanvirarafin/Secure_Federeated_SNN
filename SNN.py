fixed issues with the KL regularization. Cleaned some parts of the code

SNN.py
@@ -110,9 +110,7 @@ class SNNetwork(torch.nn.Module):
         self.feedback_potential = torch.zeros([self.n_learnable_neurons])
 
         ### Gradients
-        self.bias_gradient = torch.zeros(self.bias.shape)
-        self.ff_gradient = torch.zeros(self.feedforward_weights.shape)
-        self.fb_gradient = torch.zeros(self.feedback_weights.shape)
+        self.gradients = {'ff_weights': torch.zeros(self.feedforward_weights.shape), 'fb_weights': torch.zeros(self.feedback_weights.shape), 'bias': torch.zeros(self.bias.shape)}
 
         # Path to where the weights are saved, if None they will be saved in the current directory
         self.save_path = save_path
@@ -125,25 +123,10 @@ class SNNetwork(torch.nn.Module):
 
 
         ### Compute potential
-        feedforward_trace = torch.matmul(self.spiking_history.flip(-1), self.feedforward_filter[:, :self.spiking_history.shape[-1]].transpose(0, 1))
-        self.feedforward_potential = torch.sum(self.feedforward_weights * feedforward_trace * self.feedforward_mask, dim=(-1, -2))
-
-        feedback_trace = torch.matmul(self.spiking_history.flip(-1), self.feedback_filter[:, :self.spiking_history.shape[-1]].transpose(0, 1))
-        self.feedback_potential = torch.sum(self.feedback_weights * feedback_trace[self.learnable_neurons, :], dim=(-1))
-
-        self.potential = self.feedforward_potential + self.feedback_potential + self.bias
-
+        self.potential = self.compute_ff_potential() + self.compute_fb_potential() + self.bias
 
         ### Update spiking history
-        self.spiking_history = torch.cat((self.spiking_history[:, - self.memory_length + 1:],
-                                          torch.zeros([self.n_neurons, 1])), dim=-1)
-        self.spiking_history[self.visible_neurons, -1] = input_signal
-
-        if self.n_hidden_neurons > 0:
-            self.spiking_history[self.hidden_neurons, -1] = torch.bernoulli(torch.sigmoid(self.potential.data[self.hidden_neurons - self.n_non_learnable_neurons]))
-        if self.mode == 'test':
-            self.spiking_history[self.output_neurons, -1] = torch.bernoulli(torch.sigmoid(self.potential.data[self.output_neurons - self.n_non_learnable_neurons]))
-
+        self.spiking_history = self.update_spiking_history(input_signal)
 
         ### Compute log-probabilities
         # noinspection PyTypeChecker
@@ -155,26 +138,16 @@ class SNNetwork(torch.nn.Module):
 
 
         ### Compute gradients
-        if self.mode == 'train':
-            self.bias_gradient = self.spiking_history[self.learnable_neurons, -1] - torch.sigmoid(self.potential)
-            assert self.bias_gradient.shape == self.bias.shape, "Wrong bias gradient shape"
+        if self.mode != 'test':
+            self.gradients = self.compute_gradients(self.spiking_history[self.learnable_neurons, -1], self.potential,
+                                                    self.compute_ff_trace(self.spiking_history[:, :-1]),
+                                                    self.compute_fb_trace(self.spiking_history[:, :-1]))
 
-            self.ff_gradient = feedforward_trace.unsqueeze(0).repeat(self.n_learnable_neurons, 1, 1) \
-                               * self.bias_gradient.unsqueeze(1).repeat(1, self.n_neurons).unsqueeze(2).repeat(1, 1, self.n_basis_feedforward) \
-                               * self.feedforward_mask
-            assert self.ff_gradient.shape == self.feedforward_weights.shape, "Wrong feedforward weights gradient shape"
-
-            self.fb_gradient = feedback_trace[self.learnable_neurons, :] * self.bias_gradient.unsqueeze(1).repeat(1, self.n_basis_feedback)
-            assert self.fb_gradient.shape == self.feedback_weights.shape, "Wrong feedback weights gradient shape"
 
         return log_proba
 
 
     ### Getters
-    def get_gradients(self):
-        return {'ff_weights': self.ff_gradient, 'fb_weights': self.fb_gradient, 'bias': self.bias_gradient}
-
-
     def get_parameters(self):
         return {'ff_weights': self.feedforward_weights, 'fb_weights': self.feedback_weights, 'bias': self.bias}
 
@@ -226,6 +199,57 @@ class SNNetwork(torch.nn.Module):
             print('Mode should be one of "train" or "test"')
             raise AttributeError
         return
+
+
+    ### Computations
+    def compute_ff_trace(self, spikes):
+        return torch.matmul(spikes.flip(-1), self.feedforward_filter[:, :spikes.shape[-1]].transpose(0, 1))
+
+
+    def compute_fb_trace(self, spikes):
+        return torch.matmul(spikes.flip(-1), self.feedback_filter[:, :spikes.shape[-1]].transpose(0, 1))
+
+
+    def compute_ff_potential(self):
+        return torch.sum(self.feedforward_weights * self.compute_ff_trace(self.spiking_history) * self.feedforward_mask, dim=(-1, -2))
+
+
+    def compute_fb_potential(self):
+        return torch.sum(self.feedback_weights * self.compute_fb_trace(self.spiking_history)[self.learnable_neurons, :], dim=(-1))
+
+
+    def generate_spikes(self, spiking_history, neurons_group):
+        spiking_history[neurons_group, -1] = torch.bernoulli(torch.sigmoid(self.potential[neurons_group - self.n_non_learnable_neurons]))
+        return spiking_history
+
+
+    def update_spiking_history(self, input_signal):
+        spiking_history = torch.cat((self.spiking_history[:, - self.memory_length + 1:],
+                                          torch.zeros([self.n_neurons, 1])), dim=-1)
+        spiking_history[self.visible_neurons, -1] = input_signal
+
+        if self.n_hidden_neurons > 0:
+            spiking_history = self.generate_spikes(spiking_history, self.hidden_neurons)
+        if self.mode == 'test':
+            spiking_history = self.generate_spikes(spiking_history, self.output_neurons)
+
+        return spiking_history
+
+
+    def compute_gradients(self, spikes, potential, feedforward_trace, feedback_trace):
+        bias_gradient = spikes - torch.sigmoid(potential)
+        assert bias_gradient.shape == self.bias.shape, "Wrong bias gradient shape"
+
+        ff_gradient = feedforward_trace.unsqueeze(0).repeat(self.n_learnable_neurons, 1, 1) \
+                      * bias_gradient.unsqueeze(1).repeat(1, self.n_neurons).unsqueeze(2).repeat(1, 1, self.n_basis_feedforward) \
+                      * self.feedforward_mask
+        assert ff_gradient.shape == self.feedforward_weights.shape, "Wrong feedforward weights gradient shape"
+
+        fb_gradient = feedback_trace[self.learnable_neurons, :] * bias_gradient.unsqueeze(1).repeat(1, self.n_basis_feedback)
+        assert fb_gradient.shape == self.feedback_weights.shape, "Wrong feedback weights gradient shape"
+
+        return {'ff_weights': ff_gradient, 'fb_weights': fb_gradient, 'bias': bias_gradient}
+
 
 
     ### Misc

mnist_test.py
@@ -17,7 +17,7 @@ t0 = time.time()
 
 ### Load Data DVS
 
-dataset = os.getcwd() + r'/mnist_dvs_25ms_26pxl.hdf5'
+dataset = 'C:/Users/K1804053/PycharmProjects/datasets/mnist-dvs/mnist_dvs_25ms_26pxl_2_digits.hdf5'
 
 # The dataset is of size n_samples * n_visible_neurons * S'
 # The first 1000 samples correspond to label '1', the next 1000 correspond to '7'
@@ -30,17 +30,17 @@ training_data = torch.cat((input_train, output_train), dim=1)
 ### Network parameters
 n_input_neurons = input_train.shape[1]
 n_output_neurons = output_train.shape[1]
-n_hidden_neurons_ = [2, 4, 8, 16, 32, 64]
+n_hidden_neurons = 64
 
 
 ### Learning parameters
-learning_rate = 0.05
+learning_rate = 0.005 / max(1, n_hidden_neurons)
 epochs = 200
 eta = 0.5  # balancedness of the dataset
 epochs_test = 200
 kappa = 0.2  # learning signal and eligibility trace averaging factor
 deltas = 1  # local updates period
-r = 0.5  # Desired hidden neurons spiking rate
+r = 0.3  # Desired hidden neurons spiking rate
 alpha = 1  # learning signal regularization coefficient
 mu = 1.5  # compression factor for the raised cosine basis
 num_ite = 5  # number of iterations
@@ -56,49 +56,51 @@ n_secondary_class = epochs - n_main_class
 ### Randomly select training samples
 num_ite = 20
 
-test_accs = [[] for _ in range(len(n_hidden_neurons_))]
+# test_accs = [[] for _ in range(len(n_hidden_neurons_))]
 
 ### Run training
-for i, n_hidden_neurons in enumerate(n_hidden_neurons_):
-    print("Nh: %d" % n_hidden_neurons)
-    n_neurons = n_input_neurons + n_output_neurons + n_hidden_neurons
+# for i, n_hidden_neurons in enumerate(n_hidden_neurons_):
+#     print("Nh: %d" % n_hidden_neurons)
+n_neurons = n_input_neurons + n_output_neurons + n_hidden_neurons
 
-    num_basis_feedforward = 8
-    num_basis_feedback = 1
-    feedforward_filter = filters.raised_cosine_pillow_08
-    feedback_filter = filters.raised_cosine_pillow_08
+num_basis_feedforward = 8
+num_basis_feedback = 1
+feedforward_filter = filters.raised_cosine_pillow_08
+feedback_filter = filters.raised_cosine_pillow_08
 
-    topology = torch.tensor([[1] * n_input_neurons + [1] * n_hidden_neurons + [1] * n_output_neurons] * n_hidden_neurons +
-                            [[1] * n_input_neurons + [1] * n_hidden_neurons + [1] * n_output_neurons] * n_output_neurons)
+topology = torch.tensor([[1] * n_input_neurons + [1] * n_hidden_neurons + [1] * n_output_neurons] * n_hidden_neurons +
+                        [[1] * n_input_neurons + [1] * n_hidden_neurons + [1] * n_output_neurons] * n_output_neurons)
 
-    topology[[i for i in range(n_hidden_neurons + n_output_neurons)], [i + n_input_neurons for i in range(n_hidden_neurons + n_output_neurons)]] = 0
+topology[[i for i in range(n_hidden_neurons + n_output_neurons)], [i + n_input_neurons for i in range(n_hidden_neurons + n_output_neurons)]] = 0
 
-    for _ in range(num_ite):
-        indices = np.hstack((np.random.choice(indices_0, [n_main_class], replace=False), np.random.choice(indices_1, [n_secondary_class], replace=False)))
-        np.random.shuffle(indices)
+    # for _ in range(num_ite):
+# indices = np.hstack((np.random.choice(indices_0, [n_main_class], replace=False), np.random.choice(indices_1, [n_secondary_class], replace=False)))
+indices = np.random.choice(np.arange(len(training_data)), [epochs], replace=False)
 
-        training_sequence = training_data[indices, :, :]
-        S_prime = training_sequence.shape[-1]
-        S = epochs * S_prime
+training_sequence = training_data[indices, :, :]
+S_prime = training_sequence.shape[-1]
+S = epochs * S_prime
 
-        # Create the network
-        network = SNNetwork(n_input_neurons, n_hidden_neurons, n_output_neurons, topology,
-                            n_basis_feedforward=num_basis_feedforward, feedforward_filter=feedforward_filter,
-                            n_basis_feedback=num_basis_feedback, feedback_filter=feedback_filter,
-                            tau_ff=10, tau_fb=10, mu=mu, weights_magnitude=0.01)
+# Create the network
+network = SNNetwork(n_input_neurons, n_hidden_neurons, n_output_neurons, topology,
+                    n_basis_feedforward=num_basis_feedforward, feedforward_filter=feedforward_filter,
+                    n_basis_feedback=num_basis_feedback, feedback_filter=feedback_filter,
+                    tau_ff=10, tau_fb=10, mu=mu, weights_magnitude=0.01)
 
-        # Train it
-        train(network, training_sequence, learning_rate, kappa, deltas, r, alpha)
-        print('Number of samples trained on: %d, time: %f' % (epochs, time.time() - t0))
+# Train it
+train(network, training_sequence, learning_rate, kappa, deltas, alpha, r)
+print('Number of samples trained on: %d, time: %f' % (epochs, time.time() - t0))
 
 
-        ### Test accuracy
-        # The last 100 samples of each class are kept for test
-        test_indices = np.hstack((np.arange(900, 1000)[:epochs_test], np.arange(1900, 2000)[:epochs_test]))
-        acc, loss = get_acc_and_loss(network, dataset, test_indices)
+### Test accuracy
+# The last 100 samples of each class are kept for test
+test_indices = np.hstack((np.arange(900, 1000)[:epochs_test], np.arange(1900, 2000)[:epochs_test]))
+np.random.shuffle(test_indices)
 
-        test_accs[i].append(acc)
-        print('Final test accuracy: %f' % acc)
+acc, loss = get_acc_and_loss(network, dataset, test_indices)
 
-np.save(r'/users/k1804053/FL-SNN-distant/results/test_accs_binary.npy', test_accs)
+# test_accs[i].append(acc)
+print('Final test accuracy: %f' % acc)
+
+# np.save(r'/users/k1804053/FL-SNN-distant/results/test_accs_binary.npy', test_accs)
 

utils/training_utils.py
@@ -2,46 +2,10 @@ import torch
 import tables
 
 
-def feedforward_sampling(network, training_sequence, et, ls, s, S_prime):
-    """"
-    Runs a feedforward sampling pass:
-    - computes log probabilities
-    - accumulates learning signal
-    - accumulates eligibility trace
-    """
-    # Run forward pass
-    log_proba = network(training_sequence[int(s / S_prime), :, s % S_prime])
-
-    # Accumulate learning signal
-    ls += torch.sum(log_proba[network.output_neurons - network.n_non_learnable_neurons]) - torch.mean(log_proba[network.output_neurons - network.n_non_learnable_neurons])
-
-    # Accumulate eligibility trace
-    for parameter in et:
-        et[parameter] += network.get_gradients()[parameter]
-
-    return log_proba, ls, et
-
-
-def local_feedback_and_update(network, eligibility_trace, learning_signal, et_temp, ls_temp, learning_rate, kappa, s, deltas):
-    """"
-    Runs the local feedback and update steps:
-    - computes the learning signal
-    - updates the learning parameter
-    """
-    # At local algorithmic timesteps, do a local update
-    if (s + 1) % deltas == 0:
-        # local feedback
-        learning_signal = kappa * learning_signal + (1 - kappa) * ls_temp
-        ls_temp = 0
-
-        # Update parameter
-        for parameter in eligibility_trace:
-            eligibility_trace[parameter] = kappa * eligibility_trace[parameter] + (1 - kappa) * et_temp[parameter]
-            et_temp[parameter] = 0
-            eligibility_trace[parameter][network.hidden_neurons - network.n_non_learnable_neurons] *= learning_signal
-            network.get_parameters()[parameter] += eligibility_trace[parameter] * learning_rate
-
-    return learning_signal, ls_temp, eligibility_trace, et_temp
+def refractory_period(network):
+    length = network.memory_length + 1
+    for s in range(length):
+        network(torch.zeros([len(network.visible_neurons)], dtype=torch.float))
 
 
 def get_acc_and_loss(network, dataset, indices):
@@ -61,13 +25,20 @@ def get_acc_and_loss(network, dataset, indices):
     outputs = torch.zeros([epochs, network.n_output_neurons, S_prime])
     loss = 0
 
+    rec = torch.zeros([network.n_learnable_neurons, S_prime])
+
     for s in range(S):
         if s % S_prime == 0:
-            network.reset_internal_state()
+            print(torch.sum(rec, dim=-1))
+            print(torch.max(torch.sum(outputs[int(s / S_prime) - 1], dim=(-1)), dim=-1).indices, torch.sum(outputs[int(s / S_prime) - 1], dim=(-1)))
+            print(torch.max(torch.sum(output_sequence[int(s / S_prime) - 1], dim=(-1)), dim=-1).indices)
+
+            refractory_period(network)
 
         log_proba = network(input_sequence[int(s / S_prime), :, s % S_prime])
         loss += torch.sum(log_proba).numpy()
         outputs[int(s / S_prime), :, s % S_prime] = network.spiking_history[network.output_neurons, -1]
+        rec[:, s % S_prime] = network.spiking_history[network.learnable_neurons, -1]
 
     predictions = torch.max(torch.sum(outputs, dim=-1), dim=-1).indices
     true_classes = torch.max(torch.sum(output_sequence, dim=-1), dim=-1).indices
@@ -75,7 +46,59 @@ def get_acc_and_loss(network, dataset, indices):
     return acc, loss
 
 
-def train(network, training_sequence, learning_rate, kappa, deltas, r, alpha):
+def feedforward_sampling(network, training_sequence, et, ls, s, S_prime, alpha, r):
+    """"
+    Runs a feedforward sampling pass:
+    - computes log probabilities
+    - accumulates learning signal
+    - accumulates eligibility trace
+    """
+    # Run forward pass
+    log_proba = network(training_sequence[int(s / S_prime), :, s % S_prime])
+
+    # Accumulate learning signal
+    proba_hidden = torch.sigmoid(network.potential[network.hidden_neurons - network.n_non_learnable_neurons])
+    ls += torch.sum(log_proba[network.output_neurons - network.n_non_learnable_neurons]) / network.n_output_neurons \
+          - alpha*torch.sum(network.spiking_history[network.hidden_neurons, -1]
+          * torch.log(1e-12 + proba_hidden / r)
+          + (1 - network.spiking_history[network.hidden_neurons, -1]) * torch.log(1e-12 + (1. - proba_hidden) / (1 - r))) / network.n_hidden_neurons
+
+    # Accumulate eligibility trace
+    for parameter in et:
+        et[parameter] += network.gradients[parameter]
+
+    return log_proba, ls, et
+
+
+def local_feedback_and_update(network, eligibility_trace, learning_signal, et_temp, ls_temp, learning_rate, kappa, s, deltas):
+    """"
+    Runs the local feedback and update steps:
+    - computes the learning signal
+    - updates the learning parameter
+    """
+    # At local algorithmic timesteps, do a local update
+    if (s + 1) % deltas == 0:
+        # local feedback
+        learning_signal = kappa * learning_signal + (1 - kappa) * ls_temp
+        ls_temp = 0
+
+        # Update parameter
+        for parameter in eligibility_trace:
+            eligibility_trace[parameter] = kappa * eligibility_trace[parameter] + (1 - kappa) * et_temp[parameter]
+
+            et_temp[parameter] = 0
+
+            network.get_parameters()[parameter][network.output_neurons - network.n_non_learnable_neurons] += \
+                learning_rate * eligibility_trace[parameter][network.output_neurons - network.n_non_learnable_neurons]
+
+            network.get_parameters()[parameter][network.hidden_neurons - network.n_non_learnable_neurons] \
+                += learning_rate * learning_signal * eligibility_trace[parameter][network.hidden_neurons - network.n_non_learnable_neurons]
+
+    return learning_signal, ls_temp, eligibility_trace, et_temp
+
+
+
+def train(network, training_sequence, learning_rate, kappa, deltas, alpha, r):
     """"
     Train a network on the sequence passed as argument.
     """
@@ -95,29 +118,32 @@ def train(network, training_sequence, learning_rate, kappa, deltas, r, alpha):
     # Run the first Deltas feedforward sampling steps to initialize the learning signal and eligibility trace
     for s in range(deltas):
         # Feedforward sampling step
-        log_proba, learning_signal, eligibility_trace = feedforward_sampling(network, training_sequence, eligibility_trace, learning_signal, s, S_prime)
+        log_proba, learning_signal, eligibility_trace = feedforward_sampling(network, training_sequence, eligibility_trace, learning_signal, s, S_prime, alpha, r)
 
     # First update
     for parameter in eligibility_trace:
-        eligibility_trace[parameter][network.hidden_neurons - network.n_non_learnable_neurons] *= learning_signal
-        network.get_parameters()[parameter] += eligibility_trace[parameter] * learning_rate
+        network.get_parameters()[parameter][network.output_neurons - network.n_non_learnable_neurons] += \
+            learning_rate * eligibility_trace[parameter][network.output_neurons - network.n_non_learnable_neurons]
 
+        network.get_parameters()[parameter][network.hidden_neurons - network.n_non_learnable_neurons] \
+            += learning_rate * learning_signal * eligibility_trace[parameter][network.hidden_neurons - network.n_non_learnable_neurons]
 
     for s in range(deltas, S):
         # Reset network for each example
         if s % S_prime == 0:
-            network.reset_internal_state()
+            refractory_period(network)
 
         # Regularly decrease learning rate
-        if s % (S / 5) == 0:
-            learning_rate /= 2
+        # if s % (S / 5) == 0:
+        #     learning_rate /= 2
 
         # Feedforward sampling step
-        log_proba, ls_temp, et_temp = feedforward_sampling(network, training_sequence, et_temp, ls_temp, s, S_prime)
+        log_proba, ls_temp, et_temp = feedforward_sampling(network, training_sequence, et_temp, ls_temp, s, S_prime, alpha, r)
 
         # Local feedback and update
         learning_signal, ls_temp, eligibility_trace, et_temp \
             = local_feedback_and_update(network, eligibility_trace, learning_signal, et_temp, ls_temp, learning_rate, kappa, s, deltas)
 
-        if s % int(S / 5) == 0:
+        if s % int(S / 20) == 0:
+            print(torch.sum(network.feedforward_weights, dim=(-1, -2))[:network.n_hidden_neurons])
             print('Step %d out of %d' % (s, S))